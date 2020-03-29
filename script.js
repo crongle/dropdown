5f7ec6eaaeee33722ef16371a83cf0b68642d9e4
crongle <gasmazk73@gmail.com>
2020-03-29 19:31:04 +0100

    Initial upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const dropdown = (numberOfDropdowns) => {
+  const dropdown = {};
+
+  dropdown.setup = (numberOfDropdowns) => {
+    for (let i=1;i<=numberOfDropdowns;i++) {
+      console.log("setup entered" + i)
+      dropdownActivator = document.querySelector(".nav" + i + "-container");
+      dropdownActivator.addEventListener( "mouseover", function(){ dropdown.dropdown(i) }, false );
+      dropdownActivator.addEventListener( "mouseout", function(){ dropdown.collapse(i) }, false );
+    };
+  };
+
+  dropdown.dropdown = (reference) => {
+    dropdownList = document.querySelector(".nav" + reference);
+    dropdownList.classList.add("visible");
+  };
+
+  dropdown.collapse = (reference) => {
+    collapseList = document.querySelector(".nav" + reference);
+    collapseList.classList.remove("visible");
+  };
+
+  dropdown.setup(numberOfDropdowns);
+  return dropdown;
+};
